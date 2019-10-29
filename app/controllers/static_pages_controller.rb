1332ce3bb50ea33a18af277bedb5e18d5f3d4b54
Yoni <y_benatar@hotmail.com>
2019-10-29 02:31:50 +0100

    update on controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,44 +1,16 @@
 class StaticPagesController < ApplicationController
   
   def index
-  	puts ""
-  	puts "Helo je suis le WELCOME"
-  	puts "$" * 60 #ça affiche une ligne de 60 symboles $ facilement visible dans le terminal
-  	puts "FUUUUUCK" #tu sais que params doit s'afficher entre les 2
-  	puts "$" * 60
-  	puts ""
-  	puts ""
-  	puts ""
-  	puts ""
-  	puts "$" * 60
-  	puts "$" * 60
   	@gossips = []
   	Gossip.all.each do |my_gossip|
       @gossips << {author: my_gossip.user.first_name, title: my_gossip.title, user_id: my_gossip.user.id, gossip_id: my_gossip.id}
     end
-  	puts "$" * 60
 
   end
-=begin
-def accueil
-    @first_name = params[:first_name]
-    @gossips = []
-    Gossip.all.each do |my_gossip|
-      @gossips << {author: my_gossip.user.first_name, title: my_gossip.title, user_id: my_gossip.user.id}
-​
-    end
-  end
-
-  	users = User.where(first_name: 'David', is_admin: true).order(created_at: :desc)
-=end
-
-
-
 
   def team
   	puts "Hello je suis la page Team"
   end
-
 
   def contact
  		puts "Hello je suis la page Contact"
@@ -46,17 +18,7 @@ def accueil
 
 
   def welcome
-  	puts ""
-  	puts ""
-  	puts ""
-  	puts "Helo je suis le WELCOME"
-  	puts "$" * 60 #ça affiche une ligne de 60 symboles $ facilement visible dans le terminal
-  	puts params #tu sais que params doit s'afficher entre les 2
-  	puts "$" * 60
-  	puts ""
   	@user = params[:name]
-  	puts ""
-  	puts ""
 
   end
 
